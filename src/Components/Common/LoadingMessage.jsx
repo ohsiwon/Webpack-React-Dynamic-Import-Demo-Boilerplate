a28e8818d9503eef0abaa621d213ce85e343b73a
ohsiwon <ohsiwon@gmail.om>
2019-02-16 09:26:55 -0500

    Loading message style update

--- src/Components/Common/LoadingMessage.jsx
+++ src/Components/Common/LoadingMessage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import style from './LoadingMessage.scss?module'
 
-const LoadingMessage = () => <div>Now loading...</div>;
+const LoadingMessage = () => <div className={style.loadingMessage}>Loading...</div>;
 
 export default LoadingMessage;
